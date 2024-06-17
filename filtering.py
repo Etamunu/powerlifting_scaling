bdf470c3059e18d7f8ad23a603dad959609ce405
Etamunu <58508515+Etamunu@users.noreply.github.com>
2024-06-17 14:42:51 +0100

    Add files via upload

---
+++ filtering.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+# Replace with the path to your CSV file
+# The dataset can be downloaded at https://openpowerlifting.gitlab.io/opl-csv/bulk-csv.html
+file_path = 'openpowerlifting-2024-06-15-2f16a0ce.csv'
+
+df = pd.read_csv(file_path)
+df = df[(df['Equipment'] == 'Raw') & (df['Division'] == 'Open')]
+df = df[['Sex', 'BodyweightKg', 'Best3SquatKg', 'Best3BenchKg', 'Best3DeadliftKg', 'TotalKg']]
+df = df.dropna(subset=['Best3SquatKg', 'Best3BenchKg', 'Best3DeadliftKg', 'TotalKg', 'BodyweightKg'])
+output_file_path = 'openpower-filtered.csv'
+df.to_csv(output_file_path, index=False)
