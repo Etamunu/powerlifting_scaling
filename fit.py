13d561c049971d19992f256ebbae623ed2f4041c
Etamunu <58508515+Etamunu@users.noreply.github.com>
2024-06-20 08:27:20 +0100

    Update fit.py
    Added the line 'dfm=dfm[~np.isnan(dfm['score'])] ' , this solves the issue for the fit of the normal distribution. 
    
    The entries that produced a NaN score should be investigated.

--- fit.py
+++ fit.py
@@ -17,8 +17,8 @@ dff = df[df['Sex'] == 'F']
 def logistic(x, L, k, x0):
     return L/ (1 + np.exp(-k * (x - x0)))
 
-def myfunc(x, L, x0, k):
-    return L-np.exp(-k* (x)+x0)
+def GL(x, L, x0, k):
+    return L-np.exp(-k* x+x0)
 
 #### Gaussian noise ####
 def add_noise(data, c):
@@ -28,7 +28,7 @@ def add_noise(data, c):
 
 #### Fit the original dataset ####
 popt, pcov = curve_fit(logistic, dfm['BodyweightKg'], dfm['TotalKg'], p0=[np.max(dfm['TotalKg']) , 0.01, np.mean(dfm['BodyweightKg'])])
-popt2, pcov2 = curve_fit(myfunc, dfm['BodyweightKg'], dfm['TotalKg'], p0=[0. , 0., 0.])
+popt2, pcov2 = curve_fit(GL, dfm['BodyweightKg'], dfm['TotalKg'], p0=[0. , 0., 0.])
 
 #### Resampling ####
 kde = gaussian_kde(dfm['BodyweightKg'])
@@ -55,8 +55,9 @@ y_sampled = y[sample_indices]
 
 #### Fit the resampled dataset ####
 popt3, pcov3 = curve_fit(logistic, x_sampled, y_sampled, p0=[np.max(y) , 0.01, np.mean(x)])
-popt4, pcov4 = curve_fit(myfunc, x_sampled, y_sampled, p0=[0. , 0., 0.])
+popt4, pcov4 = curve_fit(GL, x_sampled, y_sampled, p0=[0. , 0., 0.])
 
+#### Plot fit and resampled dataset ####
 plt.plot(x_grid,kde_values_grid, label='KDE') 
 bins=np.linspace(min(dfm['BodyweightKg']), max(dfm['BodyweightKg']), 100)
 plt.hist(dfm['BodyweightKg'], bins=bins , density=True, edgecolor=(0, 0, 0, 1), facecolor=(1, 1, 1, 1), label='BW Distribution')
@@ -67,9 +68,23 @@ plt.plot(x_sampled, y_sampled, marker='+', linestyle='None', markersize=10, mark
 plt.xlabel('Bodyweight (kg)')
 plt.ylabel('Total (kg)')
 plt.plot(list(range(0, 251, 1)), logistic(list(range(0, 251, 1)), *popt), label='Logistic') 
-plt.plot(range(0,251,1), myfunc(range(0,251,1),*popt2), label='GL')
-plt.plot(list(range(0, 251, 1)), logistic(list(range(0, 251, 1)), *popt3), label='Logistic with KDE weights') 
-plt.plot(range(0,251,1), myfunc(range(0,251,1),*popt4), label='GL with KDE weights')
+plt.plot(range(0,251,1), GL(range(0,251,1),*popt2), label='GL')
+plt.plot(list(range(0, 251, 1)), logistic(list(range(0, 251, 1)), *popt3), label='Logistic (resampling)') 
+plt.plot(range(0,251,1), GL(range(0,251,1),*popt4), label='GL (resampling)')
+plt.xlim([0, 250])
+plt.gcf().set_size_inches(15, 6)
+plt.legend()
+plt.show()
+
+#### Plot fit and original dataset ####
+plt.rcParams.update({'font.size': 20})
+plt.plot(dfm['BodyweightKg'], dfm['TotalKg'], marker='+', linestyle='None', markersize=10, markeredgewidth=1, color='black')
+plt.xlabel('Bodyweight (kg)')
+plt.ylabel('Total (kg)')
+plt.plot(list(range(0, 251, 1)), logistic(list(range(0, 251, 1)), *popt), label='Logistic') 
+plt.plot(range(0,251,1), GL(range(0,251,1),*popt2), label='GL')
+plt.plot(list(range(0, 251, 1)), logistic(list(range(0, 251, 1)), *popt3), label='Logistic (resampling)') 
+plt.plot(range(0,251,1), GL(range(0,251,1),*popt4), label='GL (resampling)')
 plt.xlim([0, 250])
 plt.gcf().set_size_inches(15, 6)
 plt.legend()
@@ -82,23 +97,23 @@ plt.ylabel('Frequency')
 plt.show()
 
 #### Analysis of the score distribution (it should be centered on 1) ####
-dfm['score'] = dfm['TotalKg']/logistic(dfm['BodyweightKg'], *popt3)    
+dfm['score'] = dfm['TotalKg']/logistic(dfm['BodyweightKg'], *popt3)
+dfm=dfm[~np.isnan(dfm['score'])]  
 n, bins, patches = plt.hist(dfm['score'], bins=100, color='white', edgecolor='black', density=True)
-
-#mu, std = norm.fit(dfm['score'])
-#xmin, xmax = plt.xlim()
-#x = np.linspace(xmin, xmax, 100)
-#p = norm.pdf(x, mu, std)
-#plt.plot(x, p, 'k', linewidth=2)
+mu, std = norm.fit(dfm['score'])
+xmin, xmax = plt.xlim()
+x = np.linspace(xmin, xmax, 1000)
+p = norm.pdf(x, mu, std)
+plt.plot(x, p, 'k', linewidth=2)
 
 # Examples of athletes  
 Olivares = 1152.5/logistic(178.2, *popt3)
 plt.axvline(x=Olivares, color='red')
 plt.text(Olivares+0.01, 1, s='Jesus Olivares', rotation=90, verticalalignment='center')
-#Atwood = 838.5/logistic(73.6, *popt3)
+Atwood = 838.5/logistic(73.6, *popt3)
 #plt.axvline(x=Atwood, color='red')
 #plt.text(Atwood+0.01, 1, s='Taylor Atwood', rotation=90, verticalalignment='center')
-Tarinidis = 705/logistic(65.95, *popt3)
+Tarinidis = 707.5/logistic(65.95, *popt3)
 plt.axvline(x=Tarinidis, color='red')
 plt.text(Tarinidis+0.01, 1, s='Panagiotis Tarinidis', rotation=90, verticalalignment='center')
 Fedosienko = 669.5/logistic(58.48, *popt3)
@@ -110,11 +125,9 @@ plt.gcf().set_size_inches(15, 6)
 plt.title('Distribution of score')
 plt.show()
 
-#### Analysis of the score distribution in the IPF weight classes ####
+#### Analysis of the score distribution in the IPF weight classes (original dataset) (the boxplots should be aligned if the score is unbiased) ####
 classes = [0,59,66,74,83,93,105,120,np.max(dfm['BodyweightKg'])]
 dfm['class'] = pd.cut(dfm['BodyweightKg'], bins=classes, right=False, include_lowest=True)
 dfm.boxplot(column='score', by='class', grid=False)
-plt.show()
-plt.gcf().set_size_inches(15, 6)
-plt.title('Distribution of score')
+plt.title('Distribution of score in the IPF classes (original dataset)')
 plt.show()
